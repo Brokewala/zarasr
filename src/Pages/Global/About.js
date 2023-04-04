03e4d759176e342c564dabba9a5330b6bfa99c07
lodphin broke <96722110+Brokewala@users.noreply.github.com>
2023-04-04 14:12:42 +0200

    delete gbl

--- src/Pages/Global/About.js
+++ src/Pages/Global/About.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import AboutZara from '../../Components/Global/AboutZara/AboutZara';
 import BannerAbout from '../../Components/Global/BannerAbout/BannerAbout';
-import GlobalAboutRandonne from '../../Components/Global/GlobalAboutRandonne/GlobalAboutRandonne';
-import GlobalAboutTravel from '../../Components/Global/GlobalAboutTravel/GlobalAboutTravel';
-import GlobalAboutTretec from '../../Components/Global/GlobalAboutTretec/GlobalAboutTretec';
-import GlobalAboutZara from '../../Components/Global/GlobalAboutZara/GlobalAboutZara';
 import Layout from '../../Layout/Layout';
 import "./style/About.css";
 
@@ -17,10 +13,7 @@ export default function About() {
         <div className='About-container'>
           <BannerAbout />
           <AboutZara/>
-          {/* <GlobalAboutZara /> */}
-          {/* <GlobalAboutRandonne /> */}
-          {/* <GlobalAboutTretec /> */}
-          {/* <GlobalAboutTravel /> */}
+
         </div>
       </div>
     </Layout>
